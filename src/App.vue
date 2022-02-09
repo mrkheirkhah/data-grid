8e89b886888c9e776dcb77d0c92256bbc24221ef
mrkheirkhah <mrsoroushkheirkhah@gmail.com>
2022-02-09 20:04:50 +0330

    - project initiated let's begin ;-)

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+    <div class="wrapper">
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import '@/assets/base.css';
+</style>
